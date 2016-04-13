e9818eba47da2382100d0c21fa81c001cd171775
Skip <kevin.kjellen@live.se>
2016-04-13 11:49:37 +0200

    justerat graftitlar lite

--- Bluetooth-Dator-GUI/Histogram.cpp
+++ Bluetooth-Dator-GUI/Histogram.cpp
@@ -61,7 +61,7 @@ Histogram::Histogram(float xpos, float ypos, float width, float height, int time
 	sf::Text yHel;
 	sf::Text yHalv;
 
-	formatText(titel, fontPtr, titeltext, xpos + 11*width/20, ypos - height/10, true);
+	formatText(titel, fontPtr, titeltext, xpos + 11*width/20, ypos - height/20 - 15, true);
 	formatText(sekNoll, fontPtr, "0", xpos + width, ypos + 52 * height / 50, true);
 	formatText(sekHalv, fontPtr, std::to_string(maxTime/2), xpos + 11 * width / 20, ypos + 52 * height / 50, true);
 	formatText(sekHel, fontPtr, std::to_string(maxTime), xpos + width / 10, ypos + 52 * height / 50, true);
